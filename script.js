e15c8f9d15220429ca063473b9021bc8a46e744a
AiaRup <aiale88@gmail.com>
2018-04-14 17:53:43 +0300

    added parsley library (not functioning yet), added loadin gif

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 // Function to present the data on the html page
+var output;
+
 function createHtml(bookData, status) {
-  for (var i = 0; i < bookData.items.length; i += 1) {
-    var title = bookData.items[i].volumeInfo.title;
-    var author = bookData.items[i].volumeInfo.authors[0];
-    var description = bookData.items[i].volumeInfo.description;
-    var image = bookData.items[i].volumeInfo.imageLinks.smallThumbnail;
-    if (status === 1) {
-      $('.books').append('<div class="book">' + '<h1>' + title + '</h1>' +
-      '<p>' + description + '</p>' +
-      '<h3>Written by:' + author + '</h3>' +
-      '<img src="' + image + '"></div>' );
-    } else {
-      $('.books').append('<div class="book-list">' + '<h5><a href="#">' + title + '</a></h5>' +
-        '<span>Written by: ' + author + '</span></div>');
+  if (bookData.items !== undefined) {
+    output = '';
+    for (var i = 0; i < bookData.items.length; i += 1) {
+      var title = bookData.items[i].volumeInfo.title;
+      var author = bookData.items[i].volumeInfo.authors[0];
+      var description = bookData.items[i].volumeInfo.description;
+      var image = bookData.items[i].volumeInfo.imageLinks.thumbnail;
+      output += '<div class="book">' + '<h1>' + title + '</h1>' +
+        '<p>' + description + '</p>' + '<h3>Written by: ' + author + '</h3>' +
+        '<img src="' + image + '"></div>';
+      output += '<div class="book-listItem">' + '<h5><a href="#">' + title + '</a></h5>' +
+        '<span>Written by: ' + author + '</span></div>';
     }
+
+    $('.books').append(output);
+    if (status === 1) {
+      $('.book-listItem').hide();
+    } else {
+      $('.book').hide();
+    }
+  } else {
+    $('.books').append('<h4 class="no-result">No book was found. Try another search!</h4>');
   }
 }
 
@@ -35,6 +45,8 @@ var fetch = function (searchURL, func, status) {
 
 // EventHandler when the search button is clicked
 $('.search-book').on('click', function () {
+  // Empty the search section first
+  $('.books').empty();
   // get the value of the inputs on the page
   var isbnNum = $('#isbn').val();
   var titleInput = $('#title').val();
@@ -60,3 +72,16 @@ $('.search-book').on('click', function () {
 });
 
 // EventHandler when a title book is clicked
+$('.books').on('click', 'a', function () {
+  $(this).closest('.book-listItem').prev().show();
+  $('.book-listItem').hide();
+});
+
+// Event for showing the loading image when waiting for ajax response
+$(document).ajaxSend(function() {
+  $('#ajax-loader').show();
+});
+
+$(document).ajaxComplete(function() {
+  $('#ajax-loader').hide();
+});
